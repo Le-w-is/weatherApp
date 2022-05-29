6d341d2e925d8317bcf286e67285db4feb169cf2
Lewis <100314560+Le-w-is@users.noreply.github.com>
2022-05-29 21:48:37 +0100

    I took the functionality out of the event listners and put it into one function

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 // setting variables for elemants in the html that we are going to change
-let long;
-let lat;
 const temperatureDescription = document.querySelector('.temperature-description');
 const locationTimezone = document.querySelector('.location-timezone');
 const temperatureDegrees = document.querySelector('.temperature-degrees');
 const temperatureSection = document.querySelector('.temperature-section');
 const temperatureSpan = document.querySelector('.temperature-section span');
 const backgroundColour = document.body.style.background;
-
+const weekdays = document.querySelector('.weekdays').children
 
 // getting the day of the week as an integer
 const dayOfWeekDigit = new Date().getDay();
@@ -24,15 +22,14 @@ window.addEventListener('load', () => {
     // if getCurrantPosition is successful this function runs
     function positionFound(position) {
         console.log(position);
-        long = position.coords.longitude;
-        lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        const lat = position.coords.latitude;
 
 
         // getting access to the api and fetching the data
         const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat}%2C%20${long}?unitGroup=us&key=S6EZL7D5HV45CGB4YMWLFHUAZ&contentType=json`
         fetch(api)
             .then(response => {
-
                 return response.json();
             })
 
@@ -41,18 +38,18 @@ window.addEventListener('load', () => {
             .then(data => {
                 backGroundColour(data)
                 console.log(data);
-                const temperatureC = (data.currentConditions.temp - 30) / 2 + ''; //converting fahrenhit to celsius and turning it into a string
-                const temperatureF = (data.currentConditions.temp);
-                const icon = data.currentConditions.icon;
+                const temperatureC = (data.days[dayOfWeekDigit].temp - 30) / 2 + ''; //converting fahrenhit to celsius and turning it into a string
+                const temperatureF = (data.days[dayOfWeekDigit].temp);
+                const icon = data.days[dayOfWeekDigit].icon;
                 const description = data.description;
                 const timezone = data.timezone;
-                temperatureDegrees.textContent = temperatureC.substring(0, 5);     // stopping the string from displaying more than 5 characters
+                temperatureDegrees.textContent = temperatureC.substring(0, 4);     // stopping the string from displaying more than 5 characters
                 temperatureDescription.textContent = description;
                 locationTimezone.textContent = timezone;
                 setIcons(icon, document.querySelector('.icon'));
 
 
-                // changing the temperature displayed back and forth between C and F 
+                // changing the temperature displayed back and forth between C and F by clicking on the temperature
                 temperatureSection.addEventListener('click', () => {
                     if (temperatureSpan.textContent === 'C') {
                         temperatureSpan.textContent = 'F';
@@ -71,71 +68,28 @@ window.addEventListener('load', () => {
         locationTimezone.textContent = "Unable to access location";
         console.log(err);
     }
-
-
-    // getting the icons from the skycons pack file and setting the correct one to match the weather
-    function setIcons(icon, iconID) {
-        const skycons = new Skycons({ color: "white" });
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-        skycons.play();
-        return skycons.set(iconID, Skycons[currentIcon]);
-    }
 })
+
+// getting the icons from the skycons pack file and setting the correct one to match the weather
+function setIcons(icon, iconID) {
+    const skycons = new Skycons({ color: "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+}
 
 
 // setting variables for the days of the weeks
-const mon = document.querySelector('.mon')
-const tue = document.querySelector('.tue')
-const wed = document.querySelector('.wed')
-const thu = document.querySelector('.thu')
-const fri = document.querySelector('.fri')
-const sat = document.querySelector('.sat')
-const sun = document.querySelector('.sun')
+const mon = document.querySelector('#mon')
+const tue = document.querySelector('#tue')
+const wed = document.querySelector('#wed')
+const thu = document.querySelector('#thu')
+const fri = document.querySelector('#fri')
+const sat = document.querySelector('#sat')
+const sun = document.querySelector('#sun')
 
 
-// adding clickability to the days of the week
-// mon.addEventListener('click', () => {
-//     const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat}%2C%20${long}?unitGroup=us&key=S6EZL7D5HV45CGB4YMWLFHUAZ&contentType=json`
-//     fetch(api)
-//         .then(response => {
-//             return response.json();
-//         })
-//         .then(data => {
-//             const day = dayOfWeekDigit
-//             if (day ===  ) {
-
-//             }
-//         })
-// });
-
-
-tue.addEventListener('click', () => {
-    console.log("tue");
-});
-
-wed.addEventListener('click', () => {
-    console.log("wed");
-});
-
-thu.addEventListener('click', () => {
-    console.log("thu");
-});
-
-fri.addEventListener('click', () => {
-    console.log("fri");
-});
-
-sat.addEventListener('click', () => {
-    console.log("sat");
-});
-
-sun.addEventListener('click', () => {
-    console.log("sun");
-});
-
-
-// setting the current day to glow based
-
+// setting the current day to glow based on the integer we get from getDay()
 function dayGlow() {
     if (dayOfWeekDigit === 0) {
         sun.style.textShadow = "var(--glow)"
@@ -159,7 +113,6 @@ dayGlow();
 
 function backGroundColour(data) {
     let temp = (data.currentConditions.temp - 30) / 2;
-    console.log(temp)
     if (temp < 10) {
         document.body.style.background = "var(--cold)"
     } else if (temp > 10) {
@@ -171,3 +124,69 @@ function backGroundColour(data) {
     }
 }
 
+// setting event listners for each day of the week that will call on the future weather function
+
+mon.addEventListener('click', futureWeather);
+tue.addEventListener('click', futureWeather);
+wed.addEventListener('click', futureWeather);
+thu.addEventListener('click', futureWeather);
+fri.addEventListener('click', futureWeather);
+sat.addEventListener('click', futureWeather);
+sun.addEventListener('click', futureWeather);
+
+
+// the future weather function that sets the day that was clicked on to glow then matches 
+//the day clicked on with a day in the days array in the api data and sets the html elemants to the new data
+
+function futureWeather(day) {
+    console.log(day)
+    for (i = 0; i < weekdays.length; i++) {
+        if (day.srcElement.textContent === "Mon") {
+            weekdays[i].style.textShadow = null;
+            mon.style.textShadow = "var(--glow)";
+        } else if (day.srcElement.textContent === "Tue") {
+            weekdays[i].style.textShadow = null;
+            tue.style.textShadow = "var(--glow)";
+        } else if (day.srcElement.textContent === "Wed") {
+            weekdays[i].style.textShadow = null;
+            wed.style.textShadow = "var(--glow)";
+        } else if (day.srcElement.textContent === "Thu") {
+            weekdays[i].style.textShadow = null;
+            thu.style.textShadow = "var(--glow)";
+        } else if (day.srcElement.textContent === "Fri") {
+            weekdays[i].style.textShadow = null;
+            fri.style.textShadow = "var(--glow)";
+        } else if (day.srcElement.textContent === "Sat") {
+            weekdays[i].style.textShadow = null;
+            sat.style.textShadow = "var(--glow)";
+        } else if (day.srcElement.textContent === "Sun") {
+            weekdays[i].style.textShadow = null;
+            sun.style.textShadow = "var(--glow)";
+        }
+    }
+    const api = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat}%2C%20${long}?unitGroup=us&key=S6EZL7D5HV45CGB4YMWLFHUAZ&contentType=json`
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            backGroundColour(data)
+            for (i = 0; i < data.days.length; i++) {
+                const date = data.days[i].datetime.toString();
+                const newDate = new Date(date + "Z")
+                const newDay = newDate + ''
+                const dayString = newDay.toUpperCase().substring(0, 3);
+                if (day.srcElement.id.toUpperCase() === dayString) {
+                    const temperatureC = (data.days[i].temp - 30) / 2 + '';
+                    const temperatureF = (data.days[i].temp);
+                    const icon = data.days[i].icon;
+                    const description = data.days[i].description;
+                    const timezone = data.timezone;
+                    temperatureDegrees.textContent = temperatureC.substring(0, 4);
+                    temperatureDescription.textContent = description;
+                    locationTimezone.textContent = timezone;
+                    setIcons(icon, document.querySelector('.icon'));
+                }
+            }
+        })
+};
--- plan.md
+++ plan.md
@@ -40,26 +40,38 @@ Step 5
 
 Stretch Goals
 -changing the colour of the background depending on the temperature
+
 - look through the res object to see what other features I can import
 - add a search bar so you can search other places weather/have a list of capital citys
 - if you click on the icon 3 times it will show the sun and turn yellow for a set amount of time
 
-
 - Weekdays
+
   - in html structure make a new div at the top of body have seven children with days of the week ✅
   - in css align them properly ✅
-  - make them clickable 
-  -when clicked have the display show the data for that day of the week 
-      - days come back as an array called days with 15 indexes 
-      -array[1] is tomorrow
-      -find get the day data from the browser use it to find out which day tomorrow is
-      - assign array[1] to that day then the next 6 array indexes to the following six days
-  -have the page load on the current day of the week (don't know how?)
-  -when clicked on the day should be brighter than the others (like a soft glow)
-
-
+  - make them clickable
+    -when clicked have the display show the data for that day of the week - days come back as an array called days with 15 indexes
+    -array[1] is tomorrow
+    -find get the day data from the browser use it to find out which day tomorrow is - assign array[1] to that day then the next 6 array indexes to the following six days
+    -have the page load on the current day of the week (don't know how?)
+    -when clicked on the day should be brighter than the others (like a soft glow)
 
 - be able to click on the celsius and change it to fahrenhite. it already comes in fahrenhite, just need to change the textContent. ✅
   - add an event listener for the click of the temperature-section ✅
   - when the temperature-section is clicked it will show fahrenhite, when pushed again returns to celsius✅
-  
+
+to do
+
+- check on monday if the days of the week still lign up with the numbers used
+- figurre out the celsius fahrenhite error
+
+
+//write one function to be called upon by each button click
+// it must take in the day of the weak from the button click
+// it must set glow on the right day of the week
+// it must make sure the other days of the week textShadow is null
+// it must get the correct data from the api for that day of the week
+// - get a date string from getDay()
+// - set days 0 as the current day of the week have the next day be days 1
+// -
+// it must change the elements to show the date for that day of the week
